cdc8505dbf3e3bcd0a205c725dae6052740c78fb
MikuroXina <ryosukadnak@gmail.com>
2019-12-24 01:16:40 +0900

    Impl fetch

---
+++ pages/api/fetch.ts
@@ -0,0 +1,68 @@
+import url from 'url';
+
+import { FetchService } from '../../lib/services/fetch_service';
+import { MeetingKind, testDatas } from '../../lib/meeting';
+import { ParsedUrlQuery } from 'querystring';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type DateString = string;
+
+export type FetchParameter = {
+  kind: MeetingKind;
+  from: DateString;
+  to: DateString;
+};
+
+const validateKind = (str: any): str is MeetingKind =>
+  str === 'Regular' || str === 'Others';
+
+const validateDateString = (str: any): str is DateString =>
+  typeof str === 'string' && Date.parse(str) != NaN;
+
+const validateQuery = (query: ParsedUrlQuery): query is FetchParameter => {
+  if (!('kind' in query) || !validateKind(query.kind)) {
+    return false;
+  }
+  if (!('from' in query) || !validateDateString(query.from)) {
+    return false;
+  }
+  if (!('to' in query) || !validateDateString(query.to)) {
+    return false;
+  }
+  return true;
+};
+
+const dateFromString = (str: DateString) => new Date(str);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'GET' || req.url == null) {
+    res.status(400).end('Request Wrong');
+    return;
+  }
+  const { query } = req;
+  if (!validateQuery(query)) {
+    res.status(400).end('Parameter Wrong');
+    return;
+  }
+
+  const { kind, from, to } = query;
+  const duration: [Date, Date] = [dateFromString(from), dateFromString(to)];
+
+  FetchService(
+    {
+      askDurationToFetch: async () => duration,
+      show: async meetings => {
+        res
+          .status(200)
+          .end(
+            JSON.stringify({ meetings: meetings.filter(m => m.kind === kind) })
+          );
+      },
+    },
+    {
+      read: async (duration: [Date, Date]) => {
+        return testDatas;
+      },
+    }
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import { NextPage } from 'next';
 import { FC, Fragment } from 'react';
+import fetch from 'isomorphic-unfetch';
+import { Meeting } from '../lib/meeting';
 
-const Calendar: FC = () => <Fragment></Fragment>;
+const Calendar: FC<{ meetings: Meeting[] }> = ({ meetings }) => (
+  <Fragment>
+    <ul>
+      {meetings.map(m => (
+        <li key={m.date.toString()}>{m.name}</li>
+      ))}
+    </ul>
+  </Fragment>
+);
 
-const Index: NextPage = () => (
+const Index: NextPage<{ meetings: Meeting[] }> = ({ meetings }) => (
   <Fragment>
     <h1>部内カレンダー</h1>
-    <Calendar />
+    <Calendar meetings={meetings} />
     <button>集会を作る</button>
     <style jsx>{`
       h1 {
@@ -15,5 +25,17 @@ const Index: NextPage = () => (
     `}</style>
   </Fragment>
 );
+
+Index.getInitialProps = async (): Promise<{ meetings: Meeting[] }> => {
+  const res = await fetch(
+    'http://localhost:3000/api/fetch?kind=Others&from=2020-01-01&to=2020-12-31'
+  );
+  if (res.status !== 200) {
+    console.error('Fail to fetch the meeting datas');
+    return { meetings: [] };
+  }
+  const { meetings } = await res.json();
+  return { meetings };
+};
 
 export default Index;
