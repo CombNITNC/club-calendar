68b739f2373e611147372591138c2881126406d6
MikuroXina <ryosukadnak@gmail.com>
2019-12-23 17:45:30 +0900

    Setup

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import {NextPage} from 'next';
+import {FC, Fragment} from 'react';
+
+const Calendar: FC = () => <Fragment></Fragment>;
+
+const Index: NextPage = () => (
+  <Fragment>
+    <h1>部内カレンダー</h1>
+    <Calendar />
+    <button>集会を作る</button>
+    <style jsx>{`
+      h1 {
+        color: darkblue;
+      }
+    `}</style>
+  </Fragment>
+);
+
+export default Index;
