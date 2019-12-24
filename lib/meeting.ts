98d0d3f7008aea5df2ca36df7629aaa1d8c21185
MikuroXina <ryosukadnak@gmail.com>
2019-12-24 16:27:09 +0900

    Fix typos

--- lib/meeting.ts
+++ lib/meeting.ts
@@ -1,6 +1,7 @@
 export type MeetingKind = 'Regular' | 'Others';
 
 export type Meeting = {
+  _id?: string;
   kind: MeetingKind;
   name: string;
   date: Date;
@@ -11,19 +12,19 @@ export const testDatas: Meeting[] = [
   {
     kind: 'Others',
     name: 'エイプリルフール',
-    date: new Date("2020-04-01"),
+    date: new Date('2020-04-01'),
     expired: false,
   },
   {
     kind: 'Others',
     name: 'こどもの日',
-    date: new Date("2020-05-05"),
+    date: new Date('2020-05-05'),
     expired: false,
   },
   {
     kind: 'Regular',
     name: '第一回定例会',
-    date: new Date("2020-04-06"),
+    date: new Date('2020-04-06'),
     expired: false,
   },
 ];
--- lib/repository.ts
+++ lib/repository.ts
@@ -1,47 +1,48 @@
-import { FetchOutput } from './services/FetchService';
-import { CreateOutput } from './services/CreateService';
-import { UpdateOutput } from './services/UpdateService';
-import { AbortOutput } from './services/AbortService';
+import { FetchOutput } from './services/fetch_service';
+import { CreateOutput } from './services/create_service';
+import { UpdateOutput } from './services/update_service';
+import { AbortOutput } from './services/abort_service';
 import { Meeting, testDatas } from './meeting';
 
-export class OnMemoryRepository implements FetchOutput, CreateOutput, UpdateOutput, AbortOutput {
-  meetings: Meetings[] = testDatas;
+export class OnMemoryRepository
+  implements FetchOutput, CreateOutput, UpdateOutput, AbortOutput {
+  meetings: Meeting[] = testDatas;
   incremental: number = 0;
 
-  save: async (...meetings: Meeting[]): Promise<string[]> => {
+  save = async (...meetings: Meeting[]): Promise<string[]> => {
     const addedIds: string[] = [];
     for (const toAdd of meetings) {
       const id = this.incremental.toString();
       addedIds.push(id);
-      this.meetings.push({...toAdd, "_id": id.toString()});
+      this.meetings.push({ ...toAdd, _id: id });
       ++this.incremental;
     }
     return addedIds;
-  }
-  
-  read: async (duration: [Date, Date]) Promise<Meeting[]> => {
+  };
+
+  read = async (duration: [Date, Date]): Promise<Meeting[]> => {
     const [from, to] = duration;
-    return this.meetings.filter(m => from.getTime() <= m.date.getTime() && m.date.getTime() < to.getTime());
-  }
-  
-  update(...meetings: Meeting[]): Promise<void> {
+    return this.meetings.filter(
+      m => from.getTime() <= m.date.getTime() && m.date.getTime() < to.getTime()
+    );
+  };
+
+  update = async (...meetings: Meeting[]): Promise<void> => {
     this.meetings = [...this.meetings, ...meetings];
-  }
+  };
 }
 
-export class RealRepository implements FetchOutput, CreateOutput, UpdateOutput, AbortOutput {
-  constructor() {
-  }
+export class RealRepository
+  implements FetchOutput, CreateOutput, UpdateOutput, AbortOutput {
+  constructor() {}
 
-  save(...meetings: Meeting[]): Promise<string[]> {
-  
-  }
-  
-  read(duration: [Date, Date]): Promise<Meeting[]> {
+  async save(...meetings: Meeting[]): Promise<string[]> {
     return [];
   }
-  
-  update(...meetings: Meeting[]): Promise<void> {
-  
+
+  async read(duration: [Date, Date]): Promise<Meeting[]> {
+    return [];
   }
+
+  async update(...meetings: Meeting[]): Promise<void> {}
 }
